bc4222c0a891f38d8e42f2c094b91ec1e2465717
Sebastian Grebe <sebigrebe@gmail.com>
2024-12-13 13:50:28 +0400

    rename

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,10 @@ import { Providers } from './providers';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'BetaCustomer - Find Your First Customer | Connect with Providers and Clients Effortlessly',
-  description: "A platform designed to help users connect with their first customers or providers. Post your requirements, apply for opportunities, and kickstart your business journey with ease.",
+  title: 'BetaCustomer - Find Your First Beta Customer | Connect with Providers and Clients Effortlessly',
+  description: "A platform designed to help users connect with their First Beta Customers or providers. Post your requirements, apply for opportunities, and kickstart your business journey with ease.",
   "keywords": [
-    "first customer",
+    "First Beta Customer",
     "online tools",
     "business connections",
     "find clients",
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
   ],
   "twitter": {
     "card": "summary_large_image",
-    "title": "BetaCustomer - Find Your First Customer | Connect Effortlessly",
+    "title": "BetaCustomer - Find Your First Beta Customer | Connect Effortlessly",
     "description": "A platform for posting requirements and connecting with potential clients or providers.",
     "site": "@sebastiangrebe"
   },
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="text-center">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
             Be the Perfect Match for Your
-            <span className="text-primary block">First Customer</span>
+            <span className="text-primary block">First Beta Customer</span>
           </h1>
           <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
             Post your requirements or apply to fulfill others'. A simple, transparent platform
