7009b2c7702e226c68313b76d9d39adca8666fec
Sebastian Grebe <sebigrebe@gmail.com>
2024-12-13 18:37:13 +0400

    Add rules and posthog

---
+++ app/legal/tos/page.tsx
@@ -0,0 +1,117 @@
+export default function TOS() {
+  return (
+    <section
+      id="faq"
+      className="container py-24 sm:py-32 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8"
+    >      <div className="text-center lg:text-start space-y-6">
+      <div className="p-6 bg-gray-100 text-gray-900">
+  <h1 className="text-2xl font-bold mb-4">Terms and Conditions</h1>
+
+  <div className="space-y-6">
+    <div>
+      <h2 className="text-xl font-semibold mb-2">1. Introduction</h2>
+      <p>
+        By using BetaCustomer you confirm your acceptance of, and agree to be bound by, these terms and conditions.
+      </p>
+    </div>
+
+    <div>
+      <h2 className="text-xl font-semibold mb-2">2. Agreement to Terms and Conditions</h2>
+      <p>
+        This Agreement takes effect on the date on which you first use the BetaCustomer application.
+      </p>
+    </div>
+
+    <div>
+      <h2 className="text-xl font-semibold mb-2">3. Unlimited Access Software License with Termination Rights</h2>
+      <p>
+        The BetaCustomer Software License facilitates the acquisition of BetaCustomer software through a single purchase,
+        granting users unrestricted and perpetual access to its comprehensive functionalities. Tailored for independent
+        creators, entrepreneurs, and small businesses, BetaCustomer empowers users to create compelling web pages and
+        online portfolios.
+      </p>
+      <p>
+        This license entails a straightforward and flexible arrangement, exempting users from recurring fees or
+        subscriptions. However, it is important to acknowledge that the licensor retains the right to terminate the
+        license without conditions or prerequisites. This termination provision enables the licensor to exercise
+        control over software distribution and utilization.
+      </p>
+      <p>
+        Opting for the BetaCustomer Software License enables users to enjoy the benefits of the software while
+        recognizing the licensor&apos;s unrestricted termination rights, which provide adaptability and address potential
+        unforeseen circumstances.
+      </p>
+    </div>
+
+    <div>
+      <h2 className="text-xl font-semibold mb-2">4. Refunds</h2>
+      <p>
+        Due to the nature of digital products, the BetaCustomer boilerplate cannot be refunded or exchanged once access
+        is granted.
+      </p>
+    </div>
+
+    <div>
+      <h2 className="text-xl font-semibold mb-2">5. Disclaimer</h2>
+      <p>
+        It is not warranted that BetaCustomer will meet your requirements or that its operation will be uninterrupted or
+        error free. All express and implied warranties or conditions not stated in this Agreement (including without
+        limitation, loss of profits, loss or corruption of data, business interruption or loss of contracts), so far
+        as such exclusion or disclaimer is permitted under the applicable law are excluded and expressly disclaimed.
+        This Agreement does not affect your statutory rights.
+      </p>
+    </div>
+
+    <div>
+      <h2 className="text-xl font-semibold mb-2">6. Warranties and Limitation of Liability</h2>
+      <p>
+        BetaCustomer does not give any warranty, guarantee or other term as to the quality, fitness for purpose or
+        otherwise of the software. BetaCustomer shall not be liable to you by reason of any representation (unless
+        fraudulent), or any implied warranty, condition or other term, or any duty at common law, for any loss of
+        profit or any indirect, special or consequential loss, damage, costs, expenses or other claims (whether caused
+        by BetaCustomer&apos;s negligence or the negligence of its servants or agents or otherwise) which arise out of or in
+        connection with the provision of any goods or services by BetaCustomer.
+      </p>
+      <p>
+        BetaCustomer shall not be liable or deemed to be in breach of contract by reason of any delay in performing, or
+        failure to perform, any of its obligations if the delay or failure was due to any cause beyond its reasonable
+        control. Notwithstanding contrary clauses in this Agreement, in the event that BetaCustomer are deemed liable to
+        you for breach of this Agreement, you agree that BetaCustomer&apos;s liability is limited to the amount actually paid
+        by you for your services or software, which amount calculated in reliance upon this clause. You hereby release
+        BetaCustomer from any and all obligations, liabilities and claims in excess of this limitation.
+      </p>
+    </div>
+
+    <div>
+      <h2 className="text-xl font-semibold mb-2">7. Responsibilities</h2>
+      <p>
+        BetaCustomer is not responsible for what the user does with the user-generated content.
+      </p>
+    </div>
+
+    <div>
+      <h2 className="text-xl font-semibold mb-2">8. Price Adjustments</h2>
+      <p>
+        As we continue to improve BetaCustomer and expand our offerings, the price may increase. The discount is provided
+        to help customers secure the current price without being surprised by future increases.
+      </p>
+    </div>
+
+    <div>
+      <h2 className="text-xl font-semibold mb-2">9. General Terms and Law</h2>
+      <p>
+        This Agreement is governed by the laws of Singapore. You acknowledge that no joint venture, partnership,
+        employment, or agency relationship exists between you and BetaCustomer as a result of your use of these services.
+        You agree not to hold yourself out as a representative, agent or employee of BetaCustomer. You agree that
+        BetaCustomer will not be liable by reason of any representation, act or omission to act by you.
+      </p>
+    </div>
+
+    <p className="text-sm text-gray-600 mt-4">Last updated: 7th December 2024.</p>
+  </div>
+</div>
+
+    </div>
+    </section>
+  )
+}
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,14 @@
 "use client";
-
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
 import { SessionProvider } from 'next-auth/react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  if (typeof window !== 'undefined') {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    })
+  }
+  return <PostHogProvider client={posthog}><SessionProvider>{children}</SessionProvider></PostHogProvider>;
 }
