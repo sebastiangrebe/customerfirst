6b15b6bbdc4709e4cf7cc9ca4ae79ce5b6162ab3
Sebastian Grebe <sebigrebe@gmail.com>
2024-12-14 17:35:18 +0400

    Go live

--- polar.ts
+++ polar.ts
@@ -2,5 +2,5 @@ import { Polar } from "@polar-sh/sdk";
 
 export const polar = new Polar({
 	accessToken: process.env.POLAR_ACCESS_TOKEN!,
-	server: "sandbox", // Use sandbox for testing purposes - otherwise use 'production' or omit this line
+	// server: "sandbox", // Use sandbox for testing purposes - otherwise use 'production' or omit this line
 });
